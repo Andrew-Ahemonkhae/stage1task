28d6f9c3505b2ebfa295086c3732c599b1e58260
Andrew <ahemonkhaeandrew@gmail.com>
2023-09-10 17:58:56 +0100

    implemented remaining requirements for task.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const d1 = new Date();
-const utcDate = d1.getUTCMilliseconds();
+const utcTime = d1.toISOString().slice(11, -1);
 const day = d1.getDay();
 const daynames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 document.getElementById("date").innerHTML = daynames[day];
 
-document.getElementById("utcdate").innerHTML = utcDate;
+document.getElementById("utctime").innerHTML = utcTime;
 
-console.log(1);
+
