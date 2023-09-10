339e0f7ba0443119f3bd3ca38e85a2d6fcdfe50c
Andrew <ahemonkhaeandrew@gmail.com>
2023-09-10 18:28:24 +0100

    minor changes

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const d1 = new Date();
-const utcTime = d1.toISOString().slice(11, -1);
-const day = d1.getDay();
-const daynames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-document.getElementById("date").innerHTML = daynames[day];
-
-document.getElementById("utctime").innerHTML = utcTime;
 
 
+
+function updateUTCTime() {
+    const d1 = new Date();
+    const utcTime = d1.toISOString().slice(11, -1);
+    document.getElementById("utctime").innerHTML = utcTime;
+
+    const day = d1.getDay();
+    const daynames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    document.getElementById("date").innerHTML = daynames[day];   
+
+}
+
+// Update the UTC time every second (1000 milliseconds)
+setInterval(updateUTCTime, 1000);
+
+// Initial call to display the UTC time immediately
+updateUTCTime()
